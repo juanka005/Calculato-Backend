44cbedad25bc52161175dad985923ad8d219dade
Juan Carlos Del Rosario <juancarlosdelrosario315@gmail.com>
2026-05-08 14:23:09 -0400

    Cambiado EnsureCreated por Migrate y agregada tabla productos

---
+++ Migrations/20260508181906_AgregarTablaProductos.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Calculato.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarTablaProductos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    CodigoBarras = table.Column<string>(type: "text", nullable: false),
+                    Costo = table.Column<decimal>(type: "numeric", nullable: false),
+                    PrecioVenta = table.Column<decimal>(type: "numeric", nullable: false),
+                    Stock = table.Column<int>(type: "integer", nullable: false),
+                    Categoria = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
